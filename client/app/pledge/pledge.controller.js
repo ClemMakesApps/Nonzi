959c79af8fb18027278acfa2259fe646b411ccad
Clement Ho <chh09a@acu.edu>
2014-10-19 11:47:48 -0500

    Added confirm pledge method

--- client/app/pledge/pledge.controller.js
+++ client/app/pledge/pledge.controller.js
@@ -1,6 +1,17 @@
 'use strict';
 
 angular.module('nonziApp')
-  .controller('PledgeCtrl', ['$scope', 'Donation', function ($scope, Donation) {
-    $scope.message = 'Hello';
+  .controller('PledgeCtrl', ['$scope', '$location', 'Donation', function ($scope, $location, Donation) {
+    console.log(Donation);
+
+    $scope.confirmPledge = function() {
+    	var pledge = {
+    		"downlineAmount":$scope.pledgeAmount,
+    		"amount":$scope.pledgeAmount,
+    		"user":$scope.name
+    	};
+
+    	Donation.save(pledge);
+    	// $location.path("/report");
+    }
 }]);
