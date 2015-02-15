b4705a52823555a6d28ef864843a8da826bef438
Djessy <medjessy@gmail.com>
2015-02-14 21:05:00 -0600

    Moved old code to other folder

--- client/app/main/main.controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('nonziApp')
-  .controller('MainCtrl', function ($scope, $location, $anchorScroll) {
-      $scope.contribute = function() {
-        $anchorScroll(0);
-        $location.path("/pledge");
-      }
-  });
