d4d1d70d08cb04fba8dae2c8525259090944361c
Clement Ho <chh09a@acu.edu>
2015-03-21 16:34:55 -0500

    Added credit card image highlighting

--- app/scripts/controllers/payment.js
+++ app/scripts/controllers/payment.js
@@ -8,9 +8,39 @@
  * Controller of the appApp
  */
 angular.module('multiplyMe')
-  .controller('PaymentCtrl',['$scope','$auth',function ($scope,$auth) {
+  .controller('PaymentCtrl',['$scope','$auth', '$timeout' ,function ($scope,$auth, $timeout) {
     $scope.payment = {}
     $scope.payment.user = {}
+
+    $scope.expirationYears = [];
+    $scope.expirationMonths = [];
+
+    $scope.generateYears = function() {
+      var thisYear = new Date().getFullYear();
+      for(var i = 0; i < 15; i++) {
+        $scope.expirationYears.push(thisYear + i);
+      }
+    }
+    $scope.generateYears();
+
+    $scope.generateMonths = function() {
+      var thisMonth = new Date().getMonth() + 1;
+
+      for(var i = 1; i <= 12; i++) {
+        var month = "0" + i;
+
+        if(i >= 10) {
+          month = i;
+        }
+
+        if(i == thisMonth) {
+          $scope.thisMonth = month;
+        }
+
+        $scope.expirationMonths.push(month);
+      }
+    }
+    $scope.generateMonths();
 
     $scope.register = function(){
       $auth.submitRegistration($scope.payment.user)
@@ -21,5 +51,63 @@ angular.module('multiplyMe')
 
       })
     }
+
+    function getMerchantProvider(cardNo){                      //cardNo is the credit card number
+      var cards = new Array();
+      cards [0] = {cardName: "visa",prefixes: "4"};
+      cards [1] = {cardName: "mastercard", prefixes: "51,52,53,54,55"};
+      cards [2] = {cardName: "discover",  prefixes: "6011,650"};
+      cards [3] = {cardName: "amex", prefixes: "34,37"};
+      var prefix
+      var cardType
+
+      for(cardType=0; cardType<cards.length; cardType++){
+          prefix = cards[cardType].prefixes.split(",");
+          for (var i=0; i<prefix.length; i++) {
+               var exp = new RegExp ("^" + prefix[i]);
+               if (exp.test (cardNo))
+                    return cards[cardType].cardName;            
+         }
+      }
+       return -1;       
+    }
+
+
+    $scope.visa = true;
+    $scope.mastercard = true;
+    $scope.amex = true;
+    $scope.discover = true;
+    $scope.highlightMerchant = function(newValue, oldValue) {
+      if(oldValue != newValue) {
+        var merchant = getMerchantProvider(newValue);
+
+        $scope.visa = false;
+        $scope.mastercard = false;
+        $scope.amex = false;
+        $scope.discover = false;
+        if(merchant != -1) {
+          $scope[merchant] = true;
+        }
+      }
+    }
+
+    $scope.highlightChallenge = function(newValue, oldValue) {
+      if(oldValue != null) {
+        $scope.highlightNext = true;
+      }
+    }
+
+
+    // var formCheckTimer = null;
+    // $scope.checkForm = function(newValue, oldValue) {
+    //   if(newValue != oldValue) {
+    //       $timeout.cancel(formCheckTimer);
+    //       var formCheckTimer = $timeout(function() {
+    //       }, 3000);
+    //     }
+    //   }
+    // }
+    $scope.$watch('payment.cardNumber', $scope.highlightMerchant);
+    $scope.$watch('password', $scope.highlightChallenge);
 
   }]);
