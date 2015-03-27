e9b378dfe0cb94bec6f1b46f82604014b27379dd
Clement Ho <chh09a@acu.edu>
2015-03-27 10:05:16 -0700

    Reorganized contribute ctrl methods

--- app/scripts/controllers/contribute.js
+++ app/scripts/controllers/contribute.js
@@ -7,57 +7,60 @@
  * # ContributeCtrl
  * Controller of the multiplyMe
  */
-angular.module('multiplyMe')
-  .controller('ContributeCtrl', function ($scope, $stateParams, $timeout) {
-  	$scope.selected = $stateParams.suggested;
+angular.module('multiplyMe').controller('ContributeCtrl', function ($scope, $stateParams, $timeout) {
+  //Initalize 
+  $scope.pledge = parseInt($stateParams.amount);
+  $scope.selectedRecurring = $stateParams.isSubscription == "true";
+  $scope.selectedPerk = -1;
+  $scope.suggestedAmount = -1; 
+  $scope.forceHideSuggested = false;  
+  $scope.callbackDelay = 3000;  //milliseconds
 
-  	$scope.suggestedDonations = [{
-      'amount': 175,
-      'recurring': true,
-      'message': 'Can help fund my trip for a month'
-    },{
-      'amount': 70,
-      'recurring': false,
-      'message': 'Can help fund my trip for a day'
-    },{
-      'amount': 20,
-      'recurring': true,
-      'message': 'Can help fund basic travel for a month'
-    }]
+	$scope.suggestedDonations = [{
+    'amount': 175,
+    'recurring': true,
+    'message': 'Can help fund my trip for a month'
+  },{
+    'amount': 70,
+    'recurring': false,
+    'message': 'Can help fund my trip for a day'
+  },{
+    'amount': 20,
+    'recurring': true,
+    'message': 'Can help fund basic travel for a month'
+  }];
 
-	$scope.suggestion = -1; 
+  //Suggestion related methods
+  $scope.calculateSuggestion = function(pledgeAmount) {
+    return Math.ceil(pledgeAmount/12);
+  }
 
-	$scope.calculateSuggestion = function(pledgeAmount) {
-		return Math.floor(pledgeAmount/12);
-	}
+  $scope.checkSuggestion = function() {
+    if($scope.pledge > 12 && ($scope.selectedPerk == null || $scope.selectedPerk == -1) && $scope.suggestedAmount == -1) {
+      $scope.suggestedAmount = $scope.calculateSuggestion($scope.pledge);
+    }
+  }
 
-	$scope.checkSuggestion = function() {
-		if($scope.pledge > 12 && ($scope.selected == null || $scope.selected == -1) && $scope.suggestion == -1) {
-			$scope.suggestion = $scope.calculateSuggestion($scope.pledge);
-		}
-	}
+  $scope.hidePromo = function() {
+    var alert = angular.element( document.querySelector( '.perk-promo' ) );
+    alert.addClass('bounceOut'); 
+    $scope.forceHideSuggested = true;   
+  }
 
+  $scope.yesPromo = function() {
+    $scope.hidePromo();
+    $scope.pledge = $scope.suggestedAmount;
+    $scope.selectedRecurring = true;
+  }
+
+  //Perk related methods
 	$scope.selectPerk = function(index) {
-		$scope.selected = index;
+		$scope.selectedPerk = index;
 		$scope.pledge = $scope.suggestedDonations[index].amount;
 		$scope.selectedRecurring = $scope.suggestedDonations[index].recurring;
 	}
 
-	$scope.loadSuggestion = function() {
-		if($scope.selected <= $scope.suggestedDonations.length) {
-			var selected = $scope.suggestedDonations[$scope.selected];
-			$scope.pledge = selected.amount;
-
-			if(selected.recurring) {
-				$scope.selectedRecurring = true;
-			} else {
-				$scope.selectedRecurring = false;
-			}
-
-		}
-	}
-
-	$scope.catchSuggestion = function(pledgeAmount, recurring) {
+	$scope.catchPerk = function(pledgeAmount, recurring) {
 		if(recurring == null) {
 			recurring = false;
 		}
@@ -73,42 +76,37 @@ angular.module('multiplyMe')
 		return -1;
 	}
 
+  //Watcher functions
 	var suggestionTimer = null;
 	$scope.watchPledgeAndRecurring = function(newValue, oldValue) {
 		if(newValue != oldValue) {
-			$scope.selected = $scope.catchSuggestion($scope.pledge, $scope.selectedRecurring);
+			$scope.selectedPerk = $scope.catchPerk($scope.pledge, $scope.selectedRecurring);
 
-			if($scope.suggestion == -1 && $scope.selectedRecurring != true) {
+			if($scope.selectedPerk == -1 && $scope.suggestedAmount == -1 && $scope.selectedRecurring != true && newValue != true) {
 				$timeout.cancel(suggestionTimer);
-				var suggestionTimer = $timeout(
-                        function() {
-							$scope.checkSuggestion();
 
-                        	var alert = angular.element( document.querySelector( '.perk-promo' ) );
-							alert.addClass('animated bounceIn');    
-                        },
-                        2000
-                    );
+        var callback = function() {
+          if($scope.selectedRecurring == false) {
+            //last minute abort if selected
+            $scope.checkSuggestion();
+            var alert = angular.element(document.querySelector( '.perk-promo' ));
+            alert.addClass('animated bounceIn');
+          }
+        }
+
+				var suggestionTimer = $timeout(callback, $scope.callbackDelay);
 			}
 		}
 	}
 
-	$scope.hidePromo = function() {
-		var alert = angular.element( document.querySelector( '.perk-promo' ) );
-		alert.addClass('bounceOut'); 
-		$scope.prevDisplayed = true;   
-	}
-
-	$scope.yesPromo = function() {
-		$scope.hidePromo();
-		$scope.pledge = $scope.suggestion;
-		$scope.selectedRecurring = true;
-	}
-
-
-
 	$scope.$watch('pledge', $scope.watchPledgeAndRecurring);
 	$scope.$watch('selectedRecurring', $scope.watchPledgeAndRecurring);
 
-	$scope.loadSuggestion();
+  $scope.init = function() {
+    $scope.selectedPerk = $scope.catchPerk($scope.pledge, $scope.selectedRecurring);
+    if($scope.selectedPerk != -1) {
+      $scope.forceHideSuggested = true;
+    }
+  }
+  $scope.init();
 });
