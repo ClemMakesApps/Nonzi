d5951b18813efa206e8c908c61c8e0eba408154b
Fraser <frasermince@gmail.com>
2015-05-05 20:48:51 -0500

    receive impact from server

--- app/scripts/controllers/sharedReceipt.js
+++ app/scripts/controllers/sharedReceipt.js
@@ -4,15 +4,11 @@ angular.module('multiplyMe')
   .controller('SharedReceiptCtrl', function ($scope, $stateParams, $rootScope, ShareTree) {
     $rootScope.title = 'Will you join ' + ShareTree.parent.name + ' in supporting the Bhatti Mines School';
 
-    console.log(ShareTree.children[0]);
+    console.log(ShareTree);
     $scope.name = ShareTree.parent.name;
     $scope.parent = ShareTree.parent;
     $scope.children = ShareTree.children;
-    $scope.impact = ShareTree.parent.donation.amount * .01;
-    for(var i = 0; i < $scope.children.length; i++){
-      console.log($scope.children[i].donation);
-      $scope.impact += ($scope.children[i].donation.amount * .01);
-    }
+    $scope.impact = ShareTree.impact * .01;
     $scope.donationId = $stateParams.donationId;
     
     console.log(!!$scope.children[0]);
