ce719c2b2a49a454aa9e2159ea1ccace62459b66
Clement Ho <chh09a@acu.edu>
2015-05-21 00:12:00 -0500

    Remove duplicate share function

--- app/scripts/controllers/account.js
+++ app/scripts/controllers/account.js
@@ -68,31 +68,6 @@ angular.module('multiplyMe')
       });
     }
 
-	
-	
-    $scope.share = function(provider){
-      if(provider === 'facebook'){
-        $window.open(
-         '//www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent($location.absUrl()),
-         'sharer', 'toolbar=0,status=0,width=500,height=500');
-      }
-      if(provider === 'twitter'){
-        $window.open(
-          '//www.twitter.com/intent/tweet?url=' + encodeURIComponent($location.absUrl()),
-          'sharer', 'toolbar=0,status=0,width=500,height=500');
-      }
-      if(provider === 'google'){
-        $window.open(
-          '//plus.google.com/share?url=' + encodeURIComponent($location.absUrl()),
-          'sharer', 'toolbar=0,status=0,width=500,height=500');
-      }
-      if(provider === 'email'){
-        // $window.open(
-        //   '//plus.google.com/share?url=' + encodeURIComponent($location.absUrl()),
-        //   'sharer', 'toolbar=0,status=0,width=500,height=500');
-      }
-    }
-
     var pledgeText = 'Pledged money to support a free school in India. They lose pledge unless 3 of my friends donate in 3 days. Join me https://amala.multiplyme.in/#!/share/' + donationId;
     var donatedText = 'Donated money to support a free school in India. Join me and make a difference https://amala.multiplyme.in/#!/share/' + donationId + ' @AmalaFoundation';
 
