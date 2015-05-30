b5b7b6067581f02b39f5cf34f3d86a226c628596
Clement Ho <chh09a@acu.edu>
2015-05-29 23:47:48 -0500

    Add temp fix for iframe issue

--- app/scripts/controllers/share.js
+++ app/scripts/controllers/share.js
@@ -8,7 +8,7 @@
 * Controller of the multiplyMe
 */
 angular.module('multiplyMe')
-.controller('ShareCtrl', function ($scope, $rootScope, $stateParams, Donation) {
+.controller('ShareCtrl', function ($scope, $rootScope, $stateParams, Donation, $location) {
 
   //Check flag
   var receiptYes = localStorage.getItem("receiptYes");
@@ -21,12 +21,17 @@ angular.module('multiplyMe')
   if(receiptYes == "true") {
     $scope.page = "receipt/" + $stateParams.donationId;
     $rootScope.title = 'Thank you ' + Donation.name + ' for supporting the Bhatti Mines School';
-  $rootScope.ogTitle = 'Contribute to the Bhatti Mines School Project';
+    $rootScope.ogTitle = 'Contribute to the Bhatti Mines School Project';
+
   } else {
     $scope.page = "sharedReceipt/" + $stateParams.donationId;
     $rootScope.title = 'Will you join ' + Donation.name + ' in supporting the Bhatti Mines School';
     $rootScope.ogTitle = $rootScope.title;
   }
+
+
+    //Temp fix for iframe issue
+    $location.path('/' + $scope.page);
 
   if(!donated) {
     $rootScope.description = 'I pledged money to support the Bhatti Mines School in India. They lose pledge unless 3 of my friends donate in 3 days. Join me!';
